3879a7b9f710537db77b320c656c9eaf4d15ef14
Priyansh Shah <shah.priyansh04@gmail.com>
2021-06-07 22:43:56 +0530

    test commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import { auth } from 'firebase';
+import { useAuth } from '../lib/auth';
 
 export default function Home() {
+  const auth = useAuth();
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,44 +14,36 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Fast Feedback
-        </h1>
+        <h1 className={styles.title}>Fast Feedback</h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
+        <button
+          onClick={(e) => {
+            auth.signinWithGithub();
+          }}
+        >
+          Sign In
+        </button>
+        <button
+          onClick={(e) => {
+            auth.signinWithGoogle();
+          }}
+        >
+          Google{' '}
+        </button>
+        <div>{auth?.user?.email}</div>
+        {auth.user && (
+          <button
+            onClick={(e) => {
+              auth.signout();
+            }}
           >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+            Sign Out
+          </button>
+        )}
       </main>
 
       <footer className={styles.footer}>
@@ -61,5 +57,5 @@ export default function Home() {
         </a>
       </footer>
     </div>
-  )
+  );
 }
